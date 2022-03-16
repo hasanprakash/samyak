bba3da02f44c83da7f2bb85cb4b695ede76ec13c
hasanprakash <hasanchadaram888@gmail.com>
2022-03-16 13:25:08 +0530

    test7

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Hasan's Change</h1>")
+    return HttpResponse("<h1>Hello BROOOoikovvknf</h1>")
