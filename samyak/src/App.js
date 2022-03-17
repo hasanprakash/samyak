d096f77a285dd419d2d900b8fc7ca9b14985b3b2
ChaitanyaNathSingh <mchaitanyanathsingh@gmail.com>
2022-03-17 11:11:37 +0530

    Conflicts Resolved

--- samyak/src/App.js
+++ samyak/src/App.js
@@ -1,27 +1,9 @@
 import './App.css';
-import React from 'react';
 
-// BOOTSTRAP
+//Bootstrap
 import './components/Bootstrap/css/bootstrap.min.css'
-import './components/Bootstrap/css/bootstrap-theme.min.css'
-import './components/Bootstrap/css/fontAwesome.css'
-import './components/Bootstrap/css/light-box.css'
-import './components/Bootstrap/css/templatemo-style.css'
-import './components/Bootstrap/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js';
-import './components/Bootstrap/js/vendor/bootstrap.min.js';
-import './components/Bootstrap/js/plugins.js';
-import './components/Bootstrap/js/main.js';
-
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import NavBar from './components/BaseComponents/NavBar';
-
-// import './components/Bootstrap/js/vendor/jquery-1.11.2.min.js';
-import './components/Bootstrap/css/bootstrap.min.css'
-import './components/Bootstrap/css/light-box.css'
-
-import './components/Bootstrap/css/templatemo-style.css'
+import './components/Bootstrap/css/light-box.css';
+import './components/Bootstrap/css/templatemo-style.css';
 import './components/Bootstrap/js/main.js';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
