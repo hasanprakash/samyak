3057d36ec6878957d07b7db88cdcb85c9a8b62d6
hasanprakash <hasanchadaram888@gmail.com>
2022-03-24 18:51:35 +0530

    static page error1

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path("users", views.ProfileView.as_view(), name="profile"),
     path("payment", views.PaymentView.as_view(), name="payment"),
     path("paymentsuccess", views.PaymentSuccessView.as_view(), name="paymentsuccess")
