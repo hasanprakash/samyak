9697eb5f9c48272e0a8bb65c36103763490718d5
hasanprakash <hasanchadaram888@gmail.com>
2022-03-24 21:34:01 +0530

    static webapp fix

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', views.test, name='test'),
+    path('test', views.test, name='test'),
     path("users", views.ProfileView.as_view(), name="profile"),
     path("payment", views.PaymentView.as_view(), name="payment"),
     path("paymentsuccess", views.PaymentSuccessView.as_view(), name="paymentsuccess")
