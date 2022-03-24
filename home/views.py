278b76fedf83c5e2c664cfb671e9056df7d33952
ChaitanyaNathSingh <mchaitanyanathsingh@gmail.com>
2022-03-25 00:06:56 +0530

    Sample

--- home/views.py
+++ home/views.py
@@ -20,7 +20,8 @@ from instamojo_wrapper import Instamojo
 from django.conf import settings
 from rest_framework.views import APIView
 
-api = Instamojo(api_key=settings.API_KEY, auth_token=settings.AUTH_TOKEN, endpoint='https://test.instamojo.com/api/1.1/')
+api = Instamojo(api_key=settings.API_KEY, auth_token=settings.AUTH_TOKEN)
+
 # Create your views here.
 
 def home(request):
