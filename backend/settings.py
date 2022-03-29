daea2ad8d9de2c9865ff3c3b55bf663b9d2155eb
hasanprakash <hasanchadaram888@gmail.com>
2022-03-29 16:54:11 +0530

    CONFLICT

--- backend/settings.py
+++ backend/settings.py
@@ -148,6 +148,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'samyak',
+        'USER': 'samyak',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -158,17 +169,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #         'PORT': '3306'
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'samyak',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
